06fbb1cc637fd25e891555944eea0d522274c135
Mary Watiri <watirimary30@gmail.com>
2024-01-24 13:06:45 +0300

    styling the home page

---
+++ home.js
@@ -0,0 +1,11 @@
+
+
+function openDonatePopup() {
+    var popup = document.getElementById('donatePopup');
+    popup.style.display = 'block';
+}
+
+function closeDonatePopup() {
+    var popup = document.getElementById('donatePopup');
+    popup.style.display = 'none';
+}
