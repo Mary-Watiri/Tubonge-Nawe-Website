5c3985f91fb62790d2cec342b4ce9ad0971aa329
Mary Watiri <watirimary30@gmail.com>
2024-01-24 14:03:00 +0300

    package.json

---
+++ login.js
@@ -0,0 +1,7 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    document.getElementById('loginPopup').style.display = 'block';
+});
+
+function closePopup() {
+    document.getElementById('loginPopup').style.display = 'none';
+}
